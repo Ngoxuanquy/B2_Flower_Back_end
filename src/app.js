70ff74d9c22721eee246e96ca842ab2f6ea50567
quynx@gemvietnam.com <quynx@gemvietnam.com>
2024-06-25 09:33:25 +0700

    fix bug

--- src/app.js
+++ src/app.js
@@ -1,30 +1,28 @@
-require('dotenv').config();
-const compression = require('compression');
-const socketIo = require('socket.io');
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
-const http = require('http');
+require("dotenv").config();
+const compression = require("compression");
+const socketIo = require("socket.io");
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const http = require("http");
 // const session = require("express-session");
-const morgan = require('morgan');
-const { checkOverload } = require('./helpers/check.connect');
-const chatController = require('./controllers/chatController');
+const morgan = require("morgan");
+const { checkOverload } = require("./helpers/check.connect");
+const chatController = require("./controllers/chatController");
 const app = express();
-const MongoStore = require('connect-mongo');
 const server = http.createServer(app);
 
 const io = socketIo(server);
 
 app.use(
-    session({
-        secret: 'your-secret-key', // Thay thế bằng một chuỗi bất kỳ để mã hóa session
-        resave: false,
-        saveUninitialized: true,
-        store: MongoStore.create({ mongoUrl: 'mongodb://localhost/your-db' }),
-        cookie: { secure: false }, // Nếu bạn chỉ sử dụng HTTPS, đặt là true
-    }),
+  session({
+    secret: "your-secret-key", // Thay thế bằng một chuỗi bất kỳ để mã hóa session
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }, // Nếu bạn chỉ sử dụng HTTPS, đặt là true
+  })
 );
 
 // init middlewares
@@ -33,38 +31,38 @@ app.use(cookieParser());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(
-    cors({
-        origin: '*', // Địa chỉ của React app
-        methods: ['GET', 'POST', 'PATCH', 'DELETE'],
-        credentials: true,
-    }),
+  cors({
+    origin: "*", // Địa chỉ của React app
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+    credentials: true,
+  })
 );
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(helmet());
 app.use(compression());
 
 // init db
 
-require('./dbs/init.mongodb');
+require("./dbs/init.mongodb");
 // checkOverload()
 
 // init routes
-app.use('/', require('./routes'));
+app.use("/", require("./routes"));
 
 app.use((req, res, next) => {
-    const error = new Error('Not Found');
-    error.status = 404;
-    next(error);
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
 });
 
 app.use((error, req, res, next) => {
-    const statusCode = error.status || 500;
-    res.status(statusCode).json({
-        status: 'error',
-        code: statusCode,
-        stack: error.stack,
-        message: error.message || 'Internal Server Error',
-    });
+  const statusCode = error.status || 500;
+  res.status(statusCode).json({
+    status: "error",
+    code: statusCode,
+    stack: error.stack,
+    message: error.message || "Internal Server Error",
+  });
 });
 
 module.exports = app;

094c158c0fbd5b520d27e933a641efa2c62a3db9
quynx@gemvietnam.com <quynx@gemvietnam.com>
2024-06-27 15:19:23 +0700

    test payos v3

--- src/routes/VnPay/index.js
+++ src/routes/VnPay/index.js
@@ -28,7 +28,7 @@ router.post("/receive-hook", async (req, res) => {
   console.log(req.body);
   res.json(req.body);
 
-  if (res.data?.desc == "Thành công") {
+  if (res?.desc == "success") {
     const newTransaction = new transaction({
       transaction_state: "active",
       userId: ORDERS.userId,

9c104a503639e2a4cc763988bd432075f8114bfd
Ngoxuanquy <ngoxuanquy1812@gmail.com>
2024-07-24 09:43:40 +0700

    add khong duyet huy hoa

--- src/services/brokenFlowers.service.js
+++ src/services/brokenFlowers.service.js
@@ -65,21 +65,24 @@ class brokenFlowersService {
       console.log({ brokenFlower });
 
       // Update the status
-      brokenFlower.status = "Đã duyệt";
+      brokenFlower.status = payload.status;
       await brokenFlower.save();
 
-      // Find the product by ID
-      const newProduct = await product.findById(payload.productId);
-      if (!newProduct) {
-        throw new Error("Product not found");
+      // If status is "Không duyệt", update product quantity
+      if (payload.status === "Đã duyệt") {
+        // Find the product by ID
+        const product = await Product.findById(payload.productId);
+        if (!product) {
+          throw new Error("Product not found");
+        }
+
+        // Update the product quantity
+        product.product_quantity -= Number(payload.quantity);
+
+        console.log({ product_quantity: product.product_quantity });
+
+        await product.save();
       }
-
-      // Update the product quantity
-      newProduct.product_quantity = newProduct.product_quantity - Number(payload.quantity);
-
-      console.log({ product_quantity: newProduct.product_quantity });
-
-      await newProduct.save();
     } catch (error) {
       console.error(error);
       throw new Error("Failed to update broken flowers.");
@@ -130,9 +133,15 @@ class brokenFlowersService {
       .lean();
   }
 
-  static async getToBrokenFlowers({ status }) {
+  static async getToBrokenFlowers({ status, status2 }) {
     console.log({ status });
-    return await brokenFlowers.find({ status: status }).lean();
+    console.log({ status2 });
+
+    return await brokenFlowers
+      .find({
+        $or: [{ status: status }, { status: status2 }],
+      })
+      .lean();
   }
 
   static async getListUserTransactiontByShop({ shopId }) {
